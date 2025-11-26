346a9b82f28ce62223104270c7de42b7bc8e4cfd
Dubrovskirill <zet10set11@gmail.com>
2025-11-26 21:56:17 +0700

    Зарегистрирован контроллер в main для UI integration

--- Conventer/main.cpp
+++ Conventer/main.cpp
@@ -1,5 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "AppController.h"
+#include "ConverterFacade.h"
+#include "PdfEngine_Poppler.h"
+#include "LibreOfficeDocEngine.h"
+#include "JobQueue.h"
+#include "StorageService.h"
 
 #include <iostream>
 
@@ -30,6 +37,16 @@ int main(int argc, char *argv[])
             if (!obj && url == objUrl)
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
+    // --- backend objects (DI) ---
+    auto* pdfEngine = new PdfEngine_Poppler();
+    auto* docEngine = new LibreOfficeDocEngine();
+    auto* queue = new JobQueue(&app);
+    auto* storage = new StorageService();
+    auto* facade = new ConverterFacade(pdfEngine, docEngine, &app);
+    auto* controller = new AppController(facade, queue, pdfEngine, docEngine, storage, &app);
+
+    engine.rootContext()->setContextProperty("appController", controller);
+
     engine.load(url);
 
     return app.exec();
